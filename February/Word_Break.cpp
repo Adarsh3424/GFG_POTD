4057ad3263c20a8772c6cd889b7f02d32db1d618
Adarsh Patil <98179909+Adarsh3424@users.noreply.github.com>
2024-02-20 22:37:08 +0530

    Create Word_Break.cpp

---
+++ February/Word_Break.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+// s : given string to search
+// dictionary : vector of available strings
+
+class Solution
+{
+public:
+    int wordBreak(int n, string s, vector<string> &dictionary) {
+        //code here
+        unordered_set<string> dict(dictionary.begin(), dictionary.end());
+        int len = s.length();
+        vector<int> dp(len + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= len; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+
+        return dp[len];
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dictionary;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dictionary.push_back(S);
+        }
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.wordBreak(n, s, dictionary)<<"\n";
+    }
+}
+
+// } Driver Code Ends
