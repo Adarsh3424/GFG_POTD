34b2379da7808d84be878d824c32e18c9493fd3e
Adarsh Patil <98179909+Adarsh3424@users.noreply.github.com>
2024-05-31 23:12:52 +0530

    Create 31-Swap-two-nibbles-in-a-byte.cpp

---
+++ May/31-Swap-two-nibbles-in-a-byte.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        bitset<8>b(n);
+        for(int i=0;i<4;i++){
+            b[i] = b[i] xor b[i+4];
+            b[i+4]= b[i+4] xor b[i];
+            b[i] = b[i] xor b[i+4];
+        }
+        return b.to_ulong();
+    }
+};
