83eae426c4f6226262c714da8769b3212a51d357
Adarsh Patil <98179909+Adarsh3424@users.noreply.github.com>
2024-06-12 23:35:12 +0530

    Create 12-Count numbers containing 4.cpp

---
+++ June/12-Count numbers containing 4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        int count = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (containsDigitFour(i)) {
+            count++;
+        }
+    }
+    return count;
+        
+    }
+    bool containsDigitFour(int num) {
+    string str = to_string(num);
+    return str.find('4') != string::npos;
+}
+};
+
