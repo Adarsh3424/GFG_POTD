48ff0349688459c1da5b2822e1787302dc0dd098
Adarsh Patil <98179909+Adarsh3424@users.noreply.github.com>
2024-07-16 21:18:46 +0530

    Create 16-Remaining String.cpp

---
+++ july/16-Remaining String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        // Your code goes here
+        int occurs = 0;
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == ch){
+                occurs++;
+            }
+            
+            if(occurs == count){
+                return s.substr(i+1);
+            }
+        }
+        return "";
+    }
+};
