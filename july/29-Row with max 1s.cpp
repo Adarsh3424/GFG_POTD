cddfa281fdb5a853a61a4da084ceec0daa22333d
Adarsh Patil <98179909+Adarsh3424@users.noreply.github.com>
2024-07-29 22:50:28 +0530

    Create 29-Row with max 1s.cpp

---
+++ july/29-Row with max 1s.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        // code here
+         int ans=-1;
+        int maxones=0;
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[0].size();j++)
+            {
+                if(arr[i][j]==1)
+                {
+                    int onescount=arr[0].size()-j;
+                    if(onescount>maxones)
+                    {
+                        ans=i;
+                        maxones=onescount;
+                    }
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
